be6bc33e242b5e82c4ebdad8e31cd5bc48f372b7
Arthur <artur.20.03@mail.ru>
2020-09-28 19:52:55 +0700

    Создал отдельные функции для рендера объектов, учёл все комментарии

--- js/stat.js
+++ js/stat.js
@@ -10,43 +10,63 @@ var FONT_GAP = 15;
 var MAIN_FONT = `16px PT Mono`;
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
-var HEADER_HEIGHT = CLOUD_Y + GAP*4 + FONT_GAP*2;
-var maxBarHeight = CLOUD_HEIGHT - GAP*6 - FONT_GAP*4;
-var namesYPosition = CLOUD_HEIGHT - 2*GAP + CLOUD_Y;
 
+var HEADER_HEIGHT = CLOUD_Y + GAP * 4 + FONT_GAP * 2;
+var MAX_BAR_HEIGHT = CLOUD_HEIGHT - GAP * 6 - FONT_GAP * 4;
+var NAMES_Y_POSITION = CLOUD_HEIGHT - GAP * 2 + CLOUD_Y;
+
+function randomSaturation() {
+  return `hsl(240, ${Math.random() * 100}%, 50%)`;
+}
+
+function renderBar(ctx, position, heightShare = 0, color) {
+  ctx.fillStyle = color;
+  var x = CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * position;
+  var y = HEADER_HEIGHT + GAP + MAX_BAR_HEIGHT * (1 - heightShare);
+  ctx.fillRect(x, y, BAR_WIDTH, MAX_BAR_HEIGHT * heightShare);
+}
+
+function getRoundMaxValue(arr) {
+  var result = arr.reduce(function (max, current) {
+    max = (current > max) ? current : max;
+    return max;
+  }, 0);
+  return Math.round(result);
+}
+
+function renderCloud(ctx, x, y, width, height, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height);
+}
+
+function renderText(ctx, x, y, text, color = `black`) {
+  ctx.font = MAIN_FONT;
+  ctx.fillStyle = color;
+  ctx.fillText(text, x, y);
+}
 
 window.renderStatistics = function (ctx, names, times) {
-
-  var namesCount = names.length;
-  var maxTime = Math.max(...times);
-  var coefficient = 0;
+  var maxTime = getRoundMaxValue(times);
+  var heightShare = 0;
   var barXPosition = 0;
+  var curTime = 0;
 
-  ctx.fillStyle = `white`;
-  ctx.shadowBlur = 5;
-  ctx.shadowOffsetX = 10;
-  ctx.shadowOffsetY = 10;
-  ctx.shadowColor = `rgba(0, 0, 0, 0.7)`;
-  ctx.fillRect(CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
-  ctx.shadowColor = `rgba(0, 0, 0, 0)`;
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_WIDTH, CLOUD_HEIGHT, `rgba(0, 0, 0, 0.7)`);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, `white`);
+
+  renderText(ctx, CLOUD_X + GAP * 2, CLOUD_Y + GAP * 3, `Ура вы победили!`);
+  renderText(ctx, CLOUD_X + GAP * 2, CLOUD_Y + GAP * 3 + FONT_GAP, `Список результатов:`);
+
+  names.forEach(function (name, position) {
+    barXPosition = CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * position;
+    curTime = Math.round(times[position]);
+    heightShare = curTime / maxTime;
+
+    renderText(ctx, barXPosition, NAMES_Y_POSITION, name);
+    renderText(ctx, barXPosition, HEADER_HEIGHT + MAX_BAR_HEIGHT * (1 - heightShare), curTime);
 
 
-  ctx.fillStyle = `black`;
-  ctx.font = MAIN_FONT;
-  ctx.fillText(`Ура вы победили!`, CLOUD_X + GAP*2, CLOUD_Y + GAP*3);
-  ctx.fillText(`Список результатов:`, CLOUD_X + GAP*2, CLOUD_Y + GAP*3 + FONT_GAP);
-
-  for (var i = 0; i < namesCount; i++) {
-    barXPosition = CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP)*i;
-    coefficient = times[i] / maxTime;
-
-    ctx.fillStyle = `black`;
-    ctx.fillText(`${names[i]}`, barXPosition, namesYPosition);
-    ctx.fillText(`${Math.round(times[i])}`, barXPosition, HEADER_HEIGHT + maxBarHeight*(1-coefficient));
-
-    ctx.fillStyle = `hsl(240,` + Math.random()*100 + `%, 50%)`;
-    if (names[i] === `Вы`)
-      ctx.fillStyle = `rgba(255, 0, 0, 1)`;
-    ctx.fillRect(barXPosition, HEADER_HEIGHT + GAP + maxBarHeight*(1-coefficient), BAR_WIDTH, maxBarHeight*coefficient);
-  }
+    var colorBar = (name === `Вы`) ? `rgba(255, 0, 0, 1)` : randomSaturation();
+    renderBar(ctx, position, heightShare, colorBar);
+  });
 };
